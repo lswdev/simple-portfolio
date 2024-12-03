71ae328471aaf4f4f9d1ec1d731d6a2128f71452
lswsepia <lswsepia@gmail.com>
2024-12-03 19:03:38 +0900

    educate 영역 추가

--- portfolio/src/views/Educate.vue
+++ portfolio/src/views/Educate.vue
@@ -1,7 +1,25 @@
 
 <template>
   <div id="education-page" style="position: relative;">
-    Educate Page
+    <div class="page-contain">
+      <div class="item-contain" style="padding-top: 4rem">
+        <div class="item-title item-grid"> Educations </div>
+        <div class="works-grid">
+          <div>
+            asdfasdf
+          </div>
+          <div>
+            asdfasdf
+          </div>
+          <div>
+            asdfasdf
+          </div>
+          <div>
+            asdfasdf
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
